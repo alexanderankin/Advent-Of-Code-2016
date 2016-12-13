b72e44bc220ceddfc594096bb7c92291894bf308
Dave Ankin <alexanderankin@gmail.com>
2016-12-13 17:41:58 -0500

    meh

---
+++ 13/cpp/Entry.cpp
@@ -0,0 +1,7 @@
+#include "Grid.h"
+
+int main(int argc, char const *argv[])
+{
+  Grid *g = new Grid();
+  return 0;
+}
---
+++ 13/cpp/Grid.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+#include "Grid.h"
+
+Grid::Grid() {
+  printf("%s\n", "whaddup");
+}
