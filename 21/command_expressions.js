e5225e095a666d60a53575cfd6b55e037f046bd3
Dave Ankin <alexanderankin@gmail.com>
2016-12-21 15:05:40 +0200

    lots of working code

---
+++ 21/command_expressions.js
@@ -0,0 +1,102 @@
+function swap_positions(input, pos_a, pos_b) {
+	var arr = input.split('');
+	var temp = arr[pos_b];
+	arr[pos_b] = arr[pos_a];
+	arr[pos_a] = temp;
+	return arr.join('');
+};
+
+// var input  = "abcdefg"
+// var output = swap_positions(input, 4, 1);
+// console.log(output);
+
+function swap_letters(input, let_a, let_b) {
+	let_b = input.indexOf(let_b);
+	let_a = input.indexOf(let_a);
+
+	var arr = input.split('');
+	var temp = arr[let_b];
+	arr[let_b] = arr[let_a];
+	arr[let_a] = temp;
+	return arr.join('');
+};
+
+// var input  = "abcdefg"
+// var output = swap_letters(input, 'a', 'f');
+// console.log(output);
+
+function rotate_number(input, direction, number) {
+	var arr = input.split('');
+	if (direction == 'right') {
+		for (var i = number - 1; i >= 0; i--) {
+			arr.unshift(arr.pop());
+		}
+	} else {
+		for (var i = number - 1; i >= 0; i--) {
+			arr.push(arr.shift());
+		}
+	}
+	return arr.join('');
+}
+
+// var input  = "abcdefg"
+// var output = rotate_number(input, 'right', 3);
+// console.log(output);
+
+function rotate_based_on_p(input, letter) {
+	// body...
+}
+
+function reverse_from_to(input, from_idx, to_idx) {
+	var arr = input.split('');
+	for (; from_idx < to_idx; (from_idx++, to_idx--)) {
+		var temp = arr[to_idx];
+		arr[to_idx] = arr[from_idx];
+		arr[from_idx] = temp;
+	}
+	return arr.join('');
+}
+
+// var input  = "abcde";
+// var output = reverse_from_to(input, 1, 3);
+// console.log(output);
+
+function insert(input, from_idx, to_idx) {
+	var arr = input.split('');
+	var ele = arr.splice(from_idx, 1)[0];
+	arr = arr.slice(0, to_idx).concat(ele).concat(arr.slice(to_idx));
+	return arr.join('');
+}
+
+var input  = "abcde";
+var output = insert(input, 1, 3);
+console.log(output);
+
+var command_expressions = {
+	swap_p: {
+		regex: /swap position ([\d]+) with position ([\d]+)/,
+		subroutine: null
+	},
+	swap_l: {
+		regex: /swap letter ([\w]) with letter ([\w])/,
+		subroutine: null
+	},
+	rotate_n: {
+		regex: /rotate (left|right) ([\d]+) step(s)*/,
+		subroutine: null
+	},
+	rotate_p: {
+		regex: /rotate based on position of letter ([\w])/,
+		subroutine: null
+	},
+	reverse: {
+		regex: /reverse positions ([\d]+) through ([\d]+)/,
+		subroutine: null
+	},
+	insert: {
+		regex: /move position ([\d]+) to position ([\d]+)/,
+		subroutine: null
+	}
+};
+
+module.exports = command_expressions;
---
+++ 21/index.js
@@ -0,0 +1,56 @@
+var fs = require('fs');
+
+function getFileContents(path, cb) {
+	fs.readFile(path, {
+		encoding: 'ascii'
+	}, cb);
+}
+
+// getFileContents('./test', function (err, data) {
+// 	console.log(data.substring(0, 10));
+// });
+
+var command_expressions = require('command_expressions');
+
+function assignCommand(line) {
+	for (i in command_expressions) {
+		if (command_expressions[i].regex.test(line)) {
+			// console.log(command_expressions[i].exec(line));
+			// console.log(command_expressions[i].regex.exec(line).slice(1));
+			// spy.call(spy, [i].concat(command_expressions[i].exec(line).slice(1)));
+			return {
+				subroutine: command_expressions[i].subroutine,
+				args: command_expressions[i].regex.exec(line).slice(1)
+			}
+		}
+	}
+}
+
+getFileContents('./test', function (err, data) {
+	var lines = data.split('\n');
+	var input = lines.shift();
+	console.log('input is', input);
+	lines.map(function match_line(line) {
+		var cmd = assignCommand(line);
+		console.log(line, cmd);
+	});
+});
+
+function spy(arguments) {
+	console.log(arguments);
+	// require('repl').start().context.a = arguments;
+}
+
+var command_commands = {
+	swap_p: /swap position ([\d]+) with position ([\d]+)/,
+	swap_l: /swap letter ([\w]) with letter ([\w])/,
+	rotate_n: /rotate (left|right) ([\d]+) steps/,
+	rotate_p: /rotate based on position of letter ([\w])/,
+	reverse: /reverse positions ([\d]+) through ([\d]+)/,
+	insert: /move position ([\d]+) to position ([\d]+)/
+};
+
+// var commands = {
+// 
+// };
+
