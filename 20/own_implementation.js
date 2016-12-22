4c071df2a79ac90f5980763208792513664d17fa
Dave Ankin <alexanderankin@gmail.com>
2016-12-22 18:15:13 +0200

    testip function works

---
+++ 20/own_implementation.js
@@ -0,0 +1,58 @@
+var fs = require('fs');
+
+function getFileContents(path, cb) {
+  fs.readFile(path, {
+    encoding: 'ascii'
+  }, cb);
+}
+
+function test_ip(ip, ranges) {
+  /*console.log("ip", ip);
+  console.log("ranges", ranges);*/
+
+  for (var i = 0; i < ranges.length; i++) {
+    /*console.log("range0", ranges[i][0], "ip", ip,
+      "range1", ranges[i][1]);*/
+
+    if (ip >= ranges[i][0] && ip <= ranges[i][1])
+      /*console.log("inside")*/
+      return false;
+  }
+  return true;
+}
+
+/*(function test_test_ip(inputfilename) {
+  getFileContents(inputfilename, function (err, data) {
+    var ivals = data
+      .split('\n')
+      .map((line) => line.split('-')
+        .map((part) => parseInt(part)))
+      .sort(function (ival_a, ival_b) {
+        return ival_a[0] - ival_b[0]
+      });
+    console.log(test_ip(8, ivals));
+  });
+})('./test');*/
+
+function main(inputfilename) {
+  getFileContents(inputfilename, function (err, data) {
+    var ivals = data
+      .split('\n')
+      .map((line) => line.split('-')
+        .map((part) => parseInt(part)))
+      .sort(function (ival_a, ival_b) {
+        return ival_a[0] - ival_b[0]
+      });
+
+    // var max_val = Math.pow(2, 32) - 1;
+
+    var minvals = ivals.map(function (ival) {
+      return ival[1] + 1;
+    });
+
+    console.log(minvals);
+    
+  });
+}
+
+// main('./test');
